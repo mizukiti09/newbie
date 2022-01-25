faef34151ad01a2cce726f6aac8e939b43293fe6
mizukiti09 <gorigoritikitiki@gmail.com>
2022-01-25 16:31:48 +0900

    Initial commit

---
+++ database/migrations/2022_01_23_231824_create_tasks_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->unsignedBigInteger('group_id')->default(0);
+            $table
+                ->foreign('group_id')
+                ->references('id')
+                ->on('groups');
+            $table->unsignedBigInteger('state_id')->default(1);
+            $table
+                ->foreign('state_id')
+                ->references('id')
+                ->on('states');
+            $table->string('task_name');
+            $table->string('task_body', 500);
+            $table->string('img1')->nullable();
+            $table->string('img2')->nullable();
+            $table->string('img3')->nullable();
+            $table
+                ->timestamp('created_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table
+                ->timestamp('updated_at')
+                ->default(
+                    DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP')
+                );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
